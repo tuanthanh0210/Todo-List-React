97d40aae8976dd55eac76fe5909a8447656aa130
ThanhLT <tuanthanh021094@gmail.com>
2020-10-16 00:43:32 +0700

    Updates

---
+++ precache-manifest.690c311bb8090e4db7240ce023cd2ed0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c670a66360150660929b551a25e6eda9",
+    "url": "/deploy-github/index.html"
+  },
+  {
+    "revision": "db93210c4d8a8f03c303",
+    "url": "/deploy-github/static/css/main.bb947ace.chunk.css"
+  },
+  {
+    "revision": "e2a25ebd34eda7e0dc34",
+    "url": "/deploy-github/static/js/2.8fd0c627.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/deploy-github/static/js/2.8fd0c627.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db93210c4d8a8f03c303",
+    "url": "/deploy-github/static/js/main.2d00bdc0.chunk.js"
+  },
+  {
+    "revision": "b6ba7e456c118c625607",
+    "url": "/deploy-github/static/js/runtime-main.a011c877.js"
+  },
+  {
+    "revision": "93fba164593c110c501ce8d2f7469533",
+    "url": "/deploy-github/static/media/add-item.93fba164.svg"
+  },
+  {
+    "revision": "8b4d6e74cffa8b09d37d84376cd22e12",
+    "url": "/deploy-github/static/media/check-done.8b4d6e74.svg"
+  },
+  {
+    "revision": "fd5e3bef5d56ff14701ee03939f13923",
+    "url": "/deploy-github/static/media/check.fd5e3bef.svg"
+  },
+  {
+    "revision": "704598b5e39b40a9586690d6ada12449",
+    "url": "/deploy-github/static/media/delete.704598b5.svg"
+  }
+]);
